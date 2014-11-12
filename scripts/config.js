4affd016e26362bfcb54b9fcf1ec1f766ebbc651
marabesi <matheus.marabesi@gmail.com>
2014-11-11 22:39:28 -0200

    Alterado caminho utilizado pelo angular para carregar os templates

--- scripts/config.js
+++ scripts/config.js
@@ -2,15 +2,15 @@
 
 angular.module('secretDev.config', ['ngRoute']).config(function ($routeProvider) {
     $routeProvider.when('/', {
-        templateUrl: '/pages/main.html',
+        templateUrl: 'pages/main.html',
         controller: 'MainCtrl'
     })
         .when('/snippet.html', {
-            templateUrl: '/pages/snippet.html',
+            templateUrl: 'pages/snippet.html',
             controller: 'Snippet'
         })
         .when('/snippet-details.html/:idSnippet', {
-            templateUrl: '/pages/snippet-details.html',
+            templateUrl: 'pages/snippet-details.html',
             controller: 'SnippetDetails'
         }).otherwise({
             redirectTo: '/'
