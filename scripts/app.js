8d79f93e137eadb52a36e7d6a03bef5614e0127f
marabesi <matheus.marabesi@gmail.com>
2014-11-11 22:28:48 -0200

    primeiro commit com o resultado do código do repositório

---
+++ scripts/app.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var URL_FIREBASE = "https://secretdev.firebaseio.com/";
+
+var secretDev = angular.module('secretDev', [
+    'secretDev.filters',
+    'secretDev.config',
+    'firebase'
+]);
+
+secretDev.controller('MainCtrl', function ($firebase, $scope) {
+    $scope.snippetLoading = true;
+
+    var ref = new Firebase(URL_FIREBASE);
+    var sync = $firebase(ref);
+
+    $scope.snippets = sync.$asArray();
+
+    $scope.snippets.$loaded(function () {
+        $scope.snippetLoading = false;
+    });
+});
+
+secretDev.controller('Snippet', function ($firebase, $scope, $location) {
+    $scope.addSnippet = function () {
+        var fireBaseSnippet = {
+            snippet: $scope.codeSnippet,
+            codeLanguage: $scope.codeLanguage,
+            createdDate: new Date().toISOString(),
+            status: {
+                parabens: 0,
+                esseEprogrameiro: 0,
+                soSucesso: 0,
+                manjaDosParanaue: 0,
+                trampoDePresidiario: 0
+            }
+        };
+
+        var ref = new Firebase(URL_FIREBASE);
+        var sync = $firebase(ref);
+
+        sync.$push(fireBaseSnippet);
+        $location.path('/');
+    };
+});
+
+secretDev.controller('SnippetDetails', function ($scope, $routeParams, $firebase) {
+    var idSnippet = $routeParams.idSnippet;
+    var ref = new Firebase(URL_FIREBASE + idSnippet);
+    var sync = $firebase(ref);
+
+    $scope.snippet = sync.$asObject();
+
+    $scope.addComment = function (isValid) {
+        if (isValid) {
+            var newComment = {
+                name: $scope.codeName,
+                comment: $scope.codeComment,
+                createdDate: new Date().toISOString()
+            }
+
+            var newCommentChild = ref.child("comments");
+            newCommentChild.push(newComment);
+
+            $scope.codeName = "";
+            $scope.codeComment = "";
+        }
+    }
+});
---
+++ scripts/filters.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('secretDev.filters', [])
+    .filter('moment', [
+    function () {
+            return function (date, method) {
+                var momented = moment(date);
+                return momented[method].apply(momented, Array.prototype.slice.call(arguments, 2));
+            };
+    }
+  ]).filter('reverse', function() {
+      function toArray(list) {
+         var k, out = [];
+         if( list ) {
+            if( angular.isArray(list) ) {
+               out = list;
+            }
+            else if( typeof(list) === 'object' ) {
+               for (k in list) {
+                  if (list.hasOwnProperty(k)) { out.push(list[k]); }
+               }
+            }
+         }
+         return out;
+      }
+      return function(items) {
+         return toArray(items).slice().reverse();
+      };
+   });
