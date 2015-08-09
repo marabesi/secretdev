7298d3866e6fa1a65f3f9d91c475ef2b5bf2e496
marabesi <matheus.marabesi@gmail.com>
2015-08-09 16:02:11 -0300

    added unit test support

---
+++ test/spec/controller/mainSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Controller: Main', function() {
+  beforeEach(module('secretDev'));
+  
+  it("Load data from firebase", function() {
+    expect(true).toBe(true);
+  });
+});
